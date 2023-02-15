0669bf96505ff9879f798cbbd1cd0039943aa5a0
chet.camlin <chester.camlin.ctr@socom.mil>
2023-02-15 17:08:07 -0500

    initial push to GitHub

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+}
+variable "subnet" {
+  description = "Name of subnet to be created."
+}
+variable "ipname" {
+  description = "Name of ip to be created."
+}
---
+++ variables.tfvars
@@ -0,0 +1,8 @@
+# Variable values. The end user would modify this file. 
+# to make thier deployment unique. 
+
+
+prefix   = "cac"
+location = "usgovvirginia"
+subnet   = "internalsub"
+ipname   = "internalip"
